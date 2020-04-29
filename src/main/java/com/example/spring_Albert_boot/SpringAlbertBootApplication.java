94f6d3b43dd8bd9524c1dc266ee5d8d53902b3e8
HP <beckalbert@ymail.com>
2020-04-29 23:40:29 +0200

    test2

---
+++ src/main/java/com/example/spring_Albert_boot/SpringAlbertBootApplication.java
@@ -0,0 +1,15 @@
+package com.example.spring_Albert_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@Configuration
+//@EnableBatchProcessing
+public class SpringAlbertBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAlbertBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_Albert_boot/SpringAlbertBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_Albert_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAlbertBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
